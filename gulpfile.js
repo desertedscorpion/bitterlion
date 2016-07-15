2fb34567074a0db5b9435436e892742302e5efe8
Emory Merryman <emory.merryman@gmail.com>
2016-07-15 10:15:06 +0000

    this lint produces errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 (function(gulp, jshint) {
     "use strict";
-    return gulp.task("lint", function() {
+    return [
+        gulp.task("lint", function() {
         return gulp.src("gulpfile.js")
             .pipe(jshint())
             .pipe(jshint.reporter("default"));
         return 5;
-    });
+    })
+    ];
 }(
     require("gulp"),
     require("gulp-jshint")
