c6f38335ae6fedee3608b54aeacb39c7b13b8a5e
Emory Merryman <emory.merryman@gmail.com>
2016-07-15 10:15:35 +0000

    this lint fixes errors

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,10 @@
     "use strict";
     return [
         gulp.task("lint", function() {
-        return gulp.src("gulpfile.js")
-            .pipe(jshint())
-            .pipe(jshint.reporter("default"));
-        return 5;
-    })
+            return gulp.src("gulpfile.js")
+                .pipe(jshint())
+                .pipe(jshint.reporter("default"));
+        })
     ];
 }(
     require("gulp"),
