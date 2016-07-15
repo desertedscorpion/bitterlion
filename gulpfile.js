e0375009be0987e8803c5e7431b62cd7980aad81
Emory Merryman <emory.merryman@gmail.com>
2016-07-15 10:42:58 +0000

    working on unit tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
-(function(gulp, jshint) {
+(function(gulp, jshint, karma) {
     "use strict";
     return [
         gulp.task("lint", function() {
             return gulp.src("*.js")
                 .pipe(jshint())
                 .pipe(jshint.reporter("default"));
+        }),
+        gulp.task("unit-test", function(){
+            return gulp.src("./foobar")
+            .pipe(karma({
+                configFile: "karma.conf.js",
+                action: "run"
+            }));
         })
     ];
 }(
     require("gulp"),
-    require("gulp-jshint")
+    require("gulp-jshint"),
+    require("gulp-karma")
 ));
---
+++ unit-test.js
@@ -0,0 +1,8 @@
+(function(){
+    "use strict";
+    return describe("Unit Tests", function(){
+        return it("fails", function(){
+            return expect(true).toBe(false);
+        });
+    });
+}());
