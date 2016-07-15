b739c70329341599432ac2bc06045424f01963ab
Emory Merryman <emory.merryman@gmail.com>
2016-07-15 10:09:17 +0000

    this lint produces errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,19 @@
-var gulp = require("gulp");
+(function() {
+    "use strict";
+    var gulp = require("gulp");
 
-// Include Our Plugins
-var jshint = require("gulp-jshint");
-var sass = require("gulp-sass");
-var concat = require("gulp-concat");
-var uglify = require("gulp-uglify");
-var rename = require("gulp-rename");
+    // Include Our Plugins
+    var jshint = require("gulp-jshint");
+    var sass = require("gulp-sass");
+    var concat = require("gulp-concat");
+    var uglify = require("gulp-uglify");
+    var rename = require("gulp-rename");
 
-// Lint Task
-       gulp.task("lint", function() {
-    return gulp.src("gulpfile.js")
-        .pipe(jshint())
-        .pipe(jshint.reporter("default"));
+    // Lint Task
+    gulp.task("lint", function() {
+        return gulp.src("gulpfile.js")
+            .pipe(jshint())
+            .pipe(jshint.reporter("default"));
         return 5;
-});
+    });
+}());
