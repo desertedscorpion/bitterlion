3fd11b3e155d209c10f76b186252e52df19cd3d1
Emory Merryman <emory.merryman@gmail.com>
2016-07-15 10:08:13 +0000

    this lint produces errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
-(function(task, src, jshint){
-    "use strict";
-    return task("lint", function(){
-        return src("gulpfile.js")
-            .pipe(jshint())
-            .pipe(jshint.reporter("default"));
-    })  
-}(
-    require("gulp").task,
-    require("gulp").src,
-    require("jshint")
-));
+var gulp = require("gulp");
+
+// Include Our Plugins
+var jshint = require("gulp-jshint");
+var sass = require("gulp-sass");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var rename = require("gulp-rename");
+
+// Lint Task
+       gulp.task("lint", function() {
+    return gulp.src("gulpfile.js")
+        .pipe(jshint())
+        .pipe(jshint.reporter("default"));
+        return 5;
+});
